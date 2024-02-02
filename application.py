89435279084532aac9af9ac4c00eaebc5f29f3ba
ayah206 <ayahluke20@gmail.com>
2024-02-02 23:50:40 +0100

    test2

--- application.py
+++ application.py
@@ -2,4 +2,4 @@ from flask import Flask
 application = Flask(__name__) 
 @application.route('/') 
 def hello_world(): 
-    return 'Hello, World! from githubactions'
+    return 'Hello, World! from github actions'
